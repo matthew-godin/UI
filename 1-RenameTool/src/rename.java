3b9173969fa8853e953646cdaba18f58fc1981da
Matthew Godin <m2godin@edu.uwaterloo.ca>
2020-01-28 06:03:46 -0500

    progress

--- 1-RenameTool/src/rename.java
+++ 1-RenameTool/src/rename.java
@@ -1,10 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class rename {
     public static void main(String[] args) {
-        new rename();
-        System.out.println("ffff");
+        final String HELP_TEXT = "help text";
+        List<String> files = new ArrayList<String>();
+        if (args.length == 0) {
+            System.out.print(HELP_TEXT);
+        }
+        else {
+            if (args[0] == "-help") {
+                System.out.print(HELP_TEXT);
+            } else if (args[0] == "-file") {
+                if (args.length == 1) {
+                    System.out.print(getBaseNumParamsMessage(
+                            "file", 1,
+                            "at least"));
+                } else {
+                    int i = 1;
+                    while (args[i].charAt(0) != '-') {
+                        files.add(args[i]);
+                    }
+                }
+            }
+        }
     }
 
-    rename() {
-
+    static String getBaseNumParamsMessage(String option, int numParams,
+                                          String numDescription) {
+        return "Invalid option: " + option + " requires " + numDescription
+                + " " + Integer.toString(numParams) + "argument"
+                + (numParams > 1 ? "s" : "") + "to be specified.";
     }
 }
