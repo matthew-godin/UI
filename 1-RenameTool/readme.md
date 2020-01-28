a8617a9eac0b239c7a6e96890f0a87fa46dae91c
Matthew Godin <m2godin@edu.uwaterloo.ca>
2020-01-28 03:17:47 -0500

    commit

---
+++ 1-RenameTool/readme.md
@@ -0,0 +1,4 @@
+Matthew Godin  
+ 
+openjdk version "11.0.6" 2020-01-14  
+Microsoft Windows 10 Home 10.0.18362 Build 18362
---
+++ 1-RenameTool/src/rename.java
@@ -0,0 +1,10 @@
+public class rename {
+    public static void main(String[] args) {
+        new rename();
+        System.out.println("ffff");
+    }
+
+    rename() {
+
+    }
+}
